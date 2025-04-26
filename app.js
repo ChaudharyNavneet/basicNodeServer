b6c4006749a80f390a4775f57e354811738014f9
navneet <nkcbhanu@gmail.com>
2025-04-26 12:20:21 +0530

    app.js changes

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const PORT = process.env.PORT || 3001;
+
 const http = require('http');
 
 const requestHandler = require('./routes');
 
 const server =  http.createServer(requestHandler)
 
-server.listen(3001);
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
